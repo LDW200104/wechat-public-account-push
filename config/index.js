985e9eb7661f8ab35e62d8d866254e9ccf5ed38a
LDW200104 <114276939+LDW200104@users.noreply.github.com>
2022-09-24 15:40:30 +0800

    Update index.js
    Signed-off-by: LDW200104 <114276939+LDW200104@users.noreply.github.com>

--- config/index.js
+++ config/index.js
@@ -6,13 +6,12 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "
-wx195a8da44c5fbbc6",
+  APP_ID: "",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "e273ec5588091504183781fd5e2d2373",
+  APP_SECRET: "",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -58,12 +57,12 @@ wx195a8da44c5fbbc6",
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [o-wN_58occUJs3fowxLkxihXh-lg
+  USERS: [o-wN_58occUJs3fowxLkxihXh-lg]
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "小歪歪",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "o-wN_58occUJs3fowxLkxihXh-lg",
       // 你想对他发送的模板消息的模板ID	F0qIbkOoRpBKMdHHbWwBcUKI7zXg_1_NG2C1jV9pNWk
       useTemplateId: "云南",
       // 所在省份
@@ -77,7 +76,7 @@ wx195a8da44c5fbbc6",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
+      festivals: []
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
@@ -86,7 +85,7 @@ wx195a8da44c5fbbc6",
         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
+      customizedDateList: []
         // 在一起的日子
         {"keyword": "love_day", date: "2022-09-30"},
         // 结婚纪念日
@@ -140,10 +139,10 @@ wx195a8da44c5fbbc6",
    */
   
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "F0qIbkOoRpBKMdHHbWwBcUKI7zXg_1_NG2C1jV9pNWk",
   
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-  CALLBACK_USERS: [
+  CALLBACK_USERS: [o-wN_58occUJs3fowxLkxihXh-lg]
     {
       // 一般都填自己
       name: "自己",
